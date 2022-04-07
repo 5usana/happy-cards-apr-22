1364ce9cfcfe336f08c0aeaf35ad643bf915b3b9
Susana Martins <susanamartins@Susanas-MacBook-Pro.local>
2022-04-06 20:43:11 -0700

    created home component

--- happy-cards-apr-22/src/App.js
+++ happy-cards-apr-22/src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
-	return <p>Hello World!</p>;
+	return (
+		<div>
+			<Home />
+		</div>
+	);
 }
 
 export default App;
