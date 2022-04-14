77f2953ffb644f8deb380886e70bc5584c0ccf2c
Susana Martins <susanamartins@Susanas-MacBook-Pro.local>
2022-04-13 21:03:57 -0700

    updated class notes

--- happy-cards-apr-22/src/App.js
+++ happy-cards-apr-22/src/App.js
@@ -1,14 +1,11 @@
 import './App.scss';
-import Home from './components/Home';
+// import Home from './components/Home';
 import HeaderFooter from './components/HeaderFooter';
 import HolidayPicker from './components/HolidayPicker';
-// import MovieDetails from './components/MovieDetails';
-// import { movieData } from './components/movieData';
 
 function App() {
 	return (
 		<div>
-			{/* <MovieDetails title={movieData.Title} type={movieData.Genre} /> */}
 			<HeaderFooter>
 				{/* <Home /> */}
 				<HolidayPicker />
--- happy-cards-apr-22/src/components/HolidayPicker.js
+++ happy-cards-apr-22/src/components/HolidayPicker.js
@@ -1,18 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const API_KEY = process.env.REACT_APP_HOLIDAYS_API_KEY;
 
 export default function HolidayPicker() {
-	const [holidayName, setHolidayName] = useState('Thanksgiving');
-	console.log('holidayName is: ', holidayName);
+	const [selectedMonth, setSelectedMonth] = useState('April');
+	const [selectedHolidayName, setSelectedHolidayName] = useState('');
+	const [holidayList, setHolidayList] = useState([]);
+
+	useEffect(() => {
+		async function getHolidays() {
+			// let monthParam = getMonthNumberByMonthName(selectedMonth);
+			const url = `https://holidays.abstractapi.com/v1/?apikey=${API_KEY}&country=US&year=2022&month=12&day=25${monthParam}`;
+  			const response = await fetch(url);
+  			const data = await response.json();
+  			console.log(data);
+			
+		}
+		getHolidays();
+	}, [selectedMonth]);
 
 	return (
 		<div>
-			<div>{holidayName}</div>
-			<button onClick={() => setHolidayName('New Years')}>
-				Change Holiday to New Years
-			</button>
-			<button onClick={() => setHolidayName('Halloween')}>
-				Change Holiday to Halloween
-			</button>
+			<div display={'flex'}>
+				<div style={{ fontWeight: 'bold' }}> Pick a Holiday</div>
+				{holidayList.map((holiday, index) => (
+					<div key={index}>{holiday.name}</div>
+				))}
+
+				<div style={{ fontWeight: 'bold' }}>Selected Holiday:</div>
+				<div>Halloween</div>
+			</div>
 		</div>
 	);
 }
