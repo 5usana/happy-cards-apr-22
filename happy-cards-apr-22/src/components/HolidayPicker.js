56ad07438bb6b759858b146096c3f4fba7695f62
Susana Martins <susanamartins@Susanas-MacBook-Pro.local>
2022-04-17 14:18:51 -0700

    updated HolidayPicker.js onClick useEffect, & removed api key

--- happy-cards-apr-22/src/components/HolidayPicker.js
+++ happy-cards-apr-22/src/components/HolidayPicker.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { getMonthNumberByMonthName } from './utils';
 
 const API_KEY = process.env.REACT_APP_HOLIDAYS_API_KEY;
@@ -10,27 +10,51 @@ export default function HolidayPicker() {
 
 	useEffect(() => {
 		async function getHolidays() {
-			// let monthParam = getMonthNumberByMonthName(selectedMonth);
-			const url = `https://holidays.abstractapi.com/v1/?apikey=${API_KEY}&country=US&year=2022&month=12&day=25${monthParam}`;
-  			const response = await fetch(url);
-  			const data = await response.json();
-  			console.log(data);
-			
+			let monthParam = getMonthNumberByMonthName(selectedMonth);
+			const url = `https://holidays.abstractapi.com/v1/?api_key=${API_KEY}&country=US&year=2022&month=${monthParam}`;
+			const response = await fetch(url);
+			const data = await response.json();
+			console.log(data);
+			setHolidayList(data);
 		}
 		getHolidays();
-	}, [selectedMonth]);
+	}, [selectedMonth]); // ALWAYS add a dependency list here, even if it's just an empty array.
 
 	return (
 		<div>
-			<div display={'flex'}>
-				<div style={{ fontWeight: 'bold' }}> Pick a Holiday</div>
-				{holidayList.map((holiday, index) => (
-					<div key={index}>{holiday.name}</div>
-				))}
-
+			<div>
+				<div style={{ fontWeight: 'bold' }}>Pick a Holiday:</div>
+				{holidayList.length > 0 &&
+					holidayList.map((holiday, index) => (
+						<div
+							style={{ cursor: 'pointer' }}
+							onClick={() => setSelectedHolidayName(holiday.name)}
+							key={index}>
+							{holiday.name}
+						</div>
+					))}
 				<div style={{ fontWeight: 'bold' }}>Selected Holiday:</div>
-				<div>Halloween</div>
+				{selectedHolidayName ? (
+					<div>{selectedHolidayName}</div>
+				) : (
+					<div>No holiday selected.</div>
+				)}
 			</div>
 		</div>
 	);
 }
+
+// function getNameAndFunctionToUpdateName(initialName) {
+//   let name = initialName;
+//   let functionToUpdateName = (newName) => {
+//     name = newName;
+//   };
+
+//   return [name, functionToUpdateName];
+// }
+// // Array Destructuring
+// let [userName, setUserName] = getNameAndFunctionToUpdateName("Andrew");
+
+// setUserName("Seena");
+
+let value = false && true;
