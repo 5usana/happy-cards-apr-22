7809f5eaac46665bc9e627e625147bd90671b530
Susana Martins <susanamartins@Susanas-MacBook-Pro.local>
2022-04-20 19:11:28 -0700

    created LabeledTextInput so that the input field is reusable

--- happy-cards-apr-22/src/components/CardForm.js
+++ happy-cards-apr-22/src/components/CardForm.js
@@ -1,30 +1,59 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { LabeledTextInput } from './LabeledTextInput';
 
 export default function CardForm() {
 	const [recipientName, setRecipientName] = useState('');
+	const [recipientEmail, setRecipientEmail] = useState('');
+	const [senderName, setSenderName] = useState('');
+	const [senderEmail, setSenderEmail] = useState('');
+	const exampleRef = useRef();
 
 	function handleSubmit(event) {
 		event.preventDefault();
 		const payload = {
-			recipientName: recipientName,
+			recipientName,
+			recipientEmail,
+			senderName,
+			senderEmail,
 		};
-		console.log('paylod is:', payload);
+		console.log('payload is: ', payload);
+		console.log('exampleRef.current.value is: ', exampleRef.current.value);
 	}
 
 	return (
 		<>
 			<form onSubmit={handleSubmit}>
+				<LabeledTextInput
+					name='Their Name'
+					value={recipientName}
+					setValue={setRecipientName}
+				/>
+				<LabeledTextInput
+					name='Their Email'
+					value={recipientEmail}
+					setValue={setRecipientEmail}
+				/>
 				<div className='Form-InputWrapper'>
-					<label className='Label' htmlFor='Their Name'>
-						Their Name
+					<div className='Label'>Pick a Holiday</div>
+					<button className='Button' type='button'>
+						Choose
+					</button>
+				</div>
+				<LabeledTextInput
+					name='Your Name'
+					value={senderName}
+					setValue={setSenderName}
+				/>
+				<LabeledTextInput
+					name='Your Email'
+					value={senderEmail}
+					setValue={setSenderEmail}
+				/>
+				<div>
+					<label htmlFor='exampleRef'>
+						Uncontrolled Input Example (using useRef())
 					</label>
-					<input
-						className='Input'
-						name='Their Name'
-						type='text'
-						value={recipientName}
-						onChange={(event) => setRecipientName(event.target.value)}
-					/>
+					<input name='exampleRef' type='text' ref={exampleRef} />
 				</div>
 				<button type='submit' className='Button'>
 					Submit
